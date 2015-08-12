3c7693edfbc14f3937342b6f04fbf45a40b2fd37
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-08-12 13:57:06 +0300

    no message

---
+++ app/code/local/Df/Paypal/Model/Config.php
@@ -0,0 +1,17 @@
+<?php
+class Df_Paypal_Model_Config extends Mage_Paypal_Model_Config {
+	/**
+	 * 2015-08-08  111
+	 * Цели перекрытия:
+	 * 1) Поддержка России в качестве страны продавца.
+	 * 2) Поддержка российского рубля в качестве валюты платежа.
+	 * @override
+	 * @see Mage_Paypal_Model_Config::__construct()
+	 * @param mixed[] $params
+	 */
+	public function __construct($params = array()) {
+		$this->_supportedCountryCodes[]= 'RU';
+		$this->_supportedCurrencyCodes[]= 'RUB';
+		parent::__construct($params);
+	}
+}
