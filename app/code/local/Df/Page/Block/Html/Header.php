cddf3a4b3ee3040f3c20dba4f972c34f5f4674b5
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-11-16 14:45:41 +0300

    2.48.4.1

--- app/code/local/Df/Page/Block/Html/Header.php
+++ app/code/local/Df/Page/Block/Html/Header.php
@@ -5,37 +5,20 @@ class Df_Page_Block_Html_Header extends Mage_Page_Block_Html_Header {
 	 * предоставление администратору возможности скрывать сообщение «Добро пожаловать»
 	 * либо показывать там только имя посетителя без фамилии.
 	 *
+	 * 2015-11-16
+	 * Начиная с Magento CE 1.7.0.2
+	 * метод @uses Mage_Page_Block_Html_Header::getWelcome() стал устаревшим:
+	 * https://github.com/OpenMage/magento-mirror/blob/1.9.2.2/app/code/core/Mage/Page/Block/Html/Header.php#L85
+	 * и уже редко используется современными оформительскими темами.
+	 * Для современных версий Magento CE
+	 * аналогичное перекрытие делает мой класс @see Df_Page_Block_Html_Welcome
+	 *
 	 * @override
 	 * @return string
 	 */
 	public function getWelcome() {
-		$result = parent::getWelcome();
-		if (
-				// Избегаем зависимости модуля Df_Page от наличия модуля Df_Tweaks
-				df_module_enabled(Df_Core_Module::TWEAKS)
-			&&
-				df_installed()
-			&&
-				df_enabled(Df_Core_Feature::TWEAKS)
-			&&
-				rm_session_customer()->isLoggedIn()
-		) {
-			if (df_cfg()->tweaks()->header()->hideWelcomeFromLoggedIn()) {
-				$result = '';
-			}
-			else {
-				if (df_cfg()->tweaks()->header()->showOnlyFirstName()) {
-					$result =
-						$this->__(
-							'Welcome, %s!'
-							, $this->escapeHtml(
-								df_h()->tweaks()->customer()->getFirstNameWithPrefix()
-							)
-						)
-					;
-				}
-			}
-		}
-		return $result;
+		/** @var string|null $result */
+		$result = Df_Page_Block_Html_Welcome::welcome();
+		return !is_null($result) ? $result : parent::_toHtml();
 	}
 }
---
+++ app/code/local/Df/Page/Block/Html/Welcome.php
@@ -0,0 +1,54 @@
+<?php
+class Df_Page_Block_Html_Welcome extends Mage_Page_Block_Html_Welcome {
+	/**
+	 * 2015-11-16
+	 * Цель перекрытия —
+	 * предоставление администратору возможности скрывать сообщение «Добро пожаловать»
+	 * либо показывать там только имя посетителя без фамилии.
+	 *
+	 * Для Magento CE версий ранее 1.7.0.2 для этой же цели используется
+	 * класс @see Df_Page_Block_Html_Head,
+	 * однако перекрываемый им метод @see Mage_Page_Block_Html_Header::getWelcome()
+	 * с Magento CE версии 1.7.0.2 стал устаревшим:
+	 * https://github.com/OpenMage/magento-mirror/blob/1.9.2.2/app/code/core/Mage/Page/Block/Html/Header.php#L85
+	 * и уже редко используется современными оформительскими темами.
+	 *
+	 * @override
+	 * @see Mage_Page_Block_Html_Welcome::_toHtml()
+	 * @return string
+	 */
+	protected function _toHtml() {
+		/** @var string|null $result */
+		$result = self::welcome();
+		return !is_null($result) ? $result : parent::_toHtml();
+	}
+
+	/**
+	 * 2015-11-16
+	 * @used-by _toHtml()
+	 * @used-by Df_Page_Block_Html_Header::getWelcome()
+	 * @return string|null
+	 */
+	public static function welcome() {
+		$result = null;
+		if (
+			// Избегаем зависимости модуля Df_Page от наличия модуля Df_Tweaks
+			df_module_enabled(Df_Core_Module::TWEAKS)
+			&& df_installed()
+			&& df_enabled(Df_Core_Feature::TWEAKS)
+			&& rm_session_customer()->isLoggedIn()
+		) {
+			if (df_cfg()->tweaks()->header()->hideWelcomeFromLoggedIn()) {
+				$result = '';
+			}
+			else {
+				if (df_cfg()->tweaks()->header()->showOnlyFirstName()) {
+					$result = sprintf(rm_translate('Welcome, %s!', 'Mage_Page'), df_escape(
+						df_h()->tweaks()->customer()->getFirstNameWithPrefix())
+					);
+				}
+			}
+		}
+		return $result;
+	}
+}
