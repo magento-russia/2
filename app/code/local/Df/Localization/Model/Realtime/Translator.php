22bf2b19fd698852d63fe06dc3e87f83b2c430a6
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-12-03 03:31:49 +0300

    no message

--- app/code/local/Df/Localization/Model/Realtime/Translator.php
+++ app/code/local/Df/Localization/Model/Realtime/Translator.php
@@ -22,7 +22,7 @@ class Df_Localization_Model_Realtime_Translator extends Df_Core_Model_Abstract {
 	/** @var bool $needLog */
 	public static $needLog = false;
 	/** @var string */
-	public static $watched = 'There are no products matching the selection.';
+	public static $watched = '';
 	/** @var bool */
 	public static $needle = false;
 
