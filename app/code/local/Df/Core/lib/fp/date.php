46d6156dfcd47f4ac9c55f7471d48fab10c24c66
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-12-04 05:34:31 +0300

    no message

--- app/code/local/Df/Core/lib/fp/date.php
+++ app/code/local/Df/Core/lib/fp/date.php
@@ -1,5 +1,40 @@
 <?php
 /**
+ * 2015-12-04
+ * @param Zend_Date $date [optional]
+ * @param string $format [optional]
+ * @param bool $showTime [optional]
+ * @return string
+ */
+function df_date_format(
+	$date = null, $format = Mage_Core_Model_Locale::FORMAT_TYPE_SHORT, $showTime = false
+) {
+	return df_mage()->coreHelper()->formatDate($date, $format, $showTime);
+}
+
+/**
+ * 2015-12-04
+ * @param Zend_Date $date [optional]
+ * @param string $format [optional]
+ * @return string
+ */
+function df_time_format($date = null, $format = Mage_Core_Model_Locale::FORMAT_TYPE_SHORT) {
+	return df_mage()->coreHelper()->formatTime($date, $format);
+}
+
+/**
+ * 2015-12-04
+ * @param Zend_Date $date [optional]
+ * @return string
+ */
+function df_date_time($date = null) {
+	return implode(' ', array(
+		df_date_format($date, Mage_Core_Model_Locale::FORMAT_TYPE_LONG)
+		,df_time_format($date, Mage_Core_Model_Locale::FORMAT_TYPE_SHORT)
+	));
+}
+
+/**
  * @param Zend_Date $date [optional]
  * @param string|null $format [optional]
  * @return string
