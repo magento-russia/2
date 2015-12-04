b0ad637e1666075db1dfc76736363c88c41acb7b
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-12-05 01:32:24 +0300

    no message

--- skin/frontend/rm/default/df/js/compiled/all.js
+++ skin/frontend/rm/default/df/js/compiled/all.js
@@ -1627,22 +1627,31 @@ rm.namespace('rm.checkout');
 	if (!rm.tweaks.options) {
 		rm.tweaks.options = {};
 	}
-	/** @type {jQuery} HTMLFormElement */
-	var $loginForm = $('#login-form');
+
+	/**
+	 * 2015-12-05
+	 * Удивительно, как я не додумался до такого решения раньше, ведь оно совсем простое.
+	 * Раньше тут стоял код var $loginForms = $('#login-form');
+	 * Конечно, новый код намного универсальнее.
+	 */
+	/** @type {jQuery} HTMLFormElement[] */
+	var $loginForms = $("form[action*='customer/account/loginPost']");
 	debugger;
-	if (0 < $loginForm.length) {
+	$loginForms.each(function() {
+		/** @type {jQuery} HTMLFormElement */
+		var $form = $(this);
 		/** @type {jQuery} HTMLInputElement */
-		var $formKeyField = $('input[name="form_key"]', $loginForm);
-		if (0 === $formKeyField.length) {
+		var $keyField = $('input[name="form_key"]', $form);
+		if (0 === $keyField.length) {
 			/** @type string */
 			var formKey = rm.tweaks.options.formKey;
 			if (formKey) {
-				$loginForm.append(
+				$form.append(
 					$('<input/>').attr({type: 'hidden', name: 'form_key', 'value': formKey})
 				);
 			}
 		}
-	}
+	});
 }); })(jQuery);function show_hide_checkbox_fields(objName,condition)
 {
 	document.getElementById(objName).disabled = condition==1?"disabled":"";
