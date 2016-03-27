42922b981e7670ee8010ecc92cbd2e28e246b00e
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-03-27 13:00:12 +0300

    2.49.3

--- app/code/local/Df/Pd4/Model/Payment.php
+++ app/code/local/Df/Pd4/Model/Payment.php
@@ -5,4 +5,32 @@ class Df_Pd4_Model_Payment extends Df_Payment_Model_Method_Base {
 	 * @return bool
 	 */
 	public function canOrder() {return true;}
+
+	/**
+	 * 2016-03-26
+	 * @override
+	 * @return bool
+	 */
+	public function canRefund() {return true;}
+
+	/**
+	 * 2016-03-26
+	 * @override
+	 * http://magento-forum.ru/topic/5394/
+	 * @param Varien_Object $payment
+	 * @param string $amount
+	 * @return Df_Payment_Model_Method_Base
+	 */
+	public function capture(Varien_Object $payment, $amount) {return $this;}
+
+	/**
+	 * 2016-03-26
+	 * @override
+	 * http://magento-forum.ru/topic/5184/
+	 * http://magento-forum.ru/topic/5394/
+	 * @param Varien_Object $payment
+	 * @param string $amount
+	 * @return Df_Payment_Model_Method_Base
+	 */
+	public function refund(Varien_Object $payment, $amount) {return $this;}
 }
