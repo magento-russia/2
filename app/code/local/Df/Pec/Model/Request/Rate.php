bfdf535da9379f3529c56f4d31210ab15cb39cdf
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-09-08 16:12:12 +0300

    2.49.9: http://magento-forum.ru/topic/5473/

--- app/code/local/Df/Pec/Model/Request/Rate.php
+++ app/code/local/Df/Pec/Model/Request/Rate.php
@@ -1,10 +1,14 @@
 <?php
 class Df_Pec_Model_Request_Rate extends Df_Shipping_Model_Request {
 	/**
+	 * 2016-09-08
+	 * «pecom.ru» => «calc.pecom.ru»
+	 * http://magento-forum.ru/topic/5473/
+	 * «ПЭК перенаправил запрос с адреса pecom.ru на неожиданный адрес calc.pecom.ru»
 	 * @override
 	 * @return string
 	 */
-	protected function getQueryHost() {return 'pecom.ru';}
+	protected function getQueryHost() {return 'calc.pecom.ru';}
 
 	/**
 	 * @override
