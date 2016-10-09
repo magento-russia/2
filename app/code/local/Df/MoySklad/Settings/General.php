4252fef9af38bb440c5d88e18d230d59db97cdb2
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-10-09 07:48:22 +0300

    no message

---
+++ app/code/local/Df/MoySklad/Settings/General.php
@@ -0,0 +1,18 @@
+<?php
+// 2016-10-09
+class Df_MoySklad_Settings_General extends Df_Core_Model_Settings {
+	/** @return boolean */
+	public function enabled() {return $this->getYesNo(__FUNCTION__);}
+	/** @return string */
+	public function login() {return $this->getString(__FUNCTION__);}
+	/** @return string */
+	public function password() {return $this->getPassword(__FUNCTION__);}
+	/**
+	 * @override
+	 * @return string
+	 */
+	protected function getKeyPrefix() {return 'df_moysklad/general/';}
+
+	/** @return Df_MoySklad_Settings_General */
+	public static function s() {static $r; return $r ? $r : $r = new self;}
+}
--- app/code/local/Df/MoySklad/controllers/IndexController.php
+++ app/code/local/Df/MoySklad/controllers/IndexController.php
@@ -3,7 +3,6 @@ class Df_MoySklad_IndexController extends Df_Core_Controller_Admin {
 	/** @return void */
 	public function indexAction() {
 		try {
-			rm_log(__METHOD__);
 			$this->loadLayout();
 			$this->renderLayout();
 		}
