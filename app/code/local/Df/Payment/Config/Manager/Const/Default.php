900722a60977d8aedb3995bb09c47bb6edd778b9
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-10-21 06:14:37 +0300

    Df_Payment_Config_Manager_Const_Default::adaptKey

--- app/code/local/Df/Payment/Config/Manager/Const/Default.php
+++ app/code/local/Df/Payment/Config/Manager/Const/Default.php
@@ -5,9 +5,7 @@ class Df_Payment_Config_Manager_Const_Default extends Df_Payment_Config_Manager_
 	 * @param string $key
 	 * @return string
 	 */
-	protected function adaptKey($key) {
-		return df_cc_path($this->getKeyBase(), 'default', parent::adaptKey($key));
-	}
+	protected function adaptKey($key) {return df_cc_path($this->getKeyBase(), 'default', $key);}
 
 	/**
 	 * @param Df_Payment_Model_Method|Df_Checkout_Module_Main $method
