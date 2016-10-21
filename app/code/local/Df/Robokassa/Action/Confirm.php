45b8faf72322a469aac06b333526a0e8d5685a92
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-10-21 06:25:12 +0300

    Модуль ROBOKASSA стал рабочим.

--- app/code/local/Df/Robokassa/Action/Confirm.php
+++ app/code/local/Df/Robokassa/Action/Confirm.php
@@ -1,7 +1,6 @@
 <?php
 class Df_Robokassa_Action_Confirm extends Df_Payment_Model_Action_Confirm {
 	/**
-	 * Использовать getConst нельзя из-за рекурсии.
 	 * @override
 	 * @return string
 	 */
@@ -21,24 +20,24 @@ class Df_Robokassa_Action_Confirm extends Df_Payment_Model_Action_Confirm {
 	protected function responseTextForSuccess() {return 'OK' . $this->rOII();}
 
 	/**
+	 * 3 октября 2012 года заметил, что Робокасса стала передавать размер заказа
+	 * с 6 знаками после запятой вместо 2.
+	 * Например, даже если в платёжном запросе написано «0.01»,
+	 * Робокасса в подтверждении указывает размер заказа как «0.010000».
+	 * Поэтому для подписи надо использовать размер заказа в том формате,
+	 * как её передает Робокасса («0.010000»)
+	 *
+	 * 2016-10-21
+	 * На сегодня описанное выше поведение системы не замечаю:
+	 * "OutSum": "14050.00"
+	 * "IncSum": "15033.50"
+	 * Хотя в любом случае вывод 2012 года верен:
+	 * для подписи надо использовать знпчение в формате Робокассы.
+	 *
 	 * @override
 	 * @return string
 	 */
-	protected function signatureOwn() {
-		/** @var string $result */
-		$result = md5(implode(':', array(
-			/**
-			 * 3 октября 2012 года заметил, что Робокасса стала передавать размер заказа
-			 * с 6 знаками после запятой вместо 2.
-			 * Например, даже если в платёжном запросе написано «0.01»,
-			 * Робокасса в подтверждении указывает размер заказа как «0.010000».
-			 * Поэтому для подписи надо использовать размер заказа в том формате,
-			 * как её передает Робокасса («0.010000»)
-			 */
-			$this->rAmountS()
-			,$this->rOII()
-			,$this->getResponsePassword()
-		)));
-		return $result;
-	}
+	protected function signatureOwn() {return md5(implode(':', [
+		$this->rAmountS(), $this->rOII(), $this->getResponsePassword()
+	]));}
 }
--- app/code/local/Df/Robokassa/controllers/ConfirmController.php
+++ app/code/local/Df/Robokassa/controllers/ConfirmController.php
@@ -1,8 +1,6 @@
 <?php
+// Платёжная система присылает сюда подтверждение приёма оплаты от покупателя.
 class Df_Robokassa_ConfirmController extends Mage_Core_Controller_Front_Action {
-	/**
-	 * Платёжная система присылает сюда подтверждение приёма оплаты от покупателя.
-	 * @return void
-	 */
+	/** @return void */
 	public function indexAction() {df_action($this);}
 }
