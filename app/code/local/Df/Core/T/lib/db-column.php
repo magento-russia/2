1c7c2505ee68c9b71551fba77ec6fc6c114094ad
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-04 02:18:06 +0300

    df_db_column_add

--- app/code/local/Df/Core/T/lib/db-column.php
+++ app/code/local/Df/Core/T/lib/db-column.php
@@ -4,15 +4,13 @@ namespace Df\Core\T\lib;
 class DbColumn extends \PHPUnit\Framework\TestCase {
 	/**
 	 * @test
-	 * 2016-11-01
+	 * 2016-11-04
 	 */
 	public function df_db_column_add() {
 		/** @var $name */
 		$name = df_uid(4, 'test_');
-		/** @var string $table */
-		$table = 'customer/customer_group';
-		df_db_column_add($table, $name);
-		$this->assertTrue(df_db_column_exists($table, $name));
+		df_db_column_add(self::$TABLE, $name);
+		$this->assertTrue(df_db_column_exists(self::$TABLE, $name));
 	}
 
 	/**
@@ -20,10 +18,8 @@ class DbColumn extends \PHPUnit\Framework\TestCase {
 	 * 2016-11-01
 	 */
 	public function df_db_column_exists() {
-		/** @var string $table */
-		$table = 'customer/customer_group';
-		$this->assertTrue(df_db_column_exists($table, 'customer_group_id'));
-		$this->assertFalse(df_db_column_exists($table, 'non_existent_column'));
+		$this->assertTrue(df_db_column_exists(self::$TABLE, 'customer_group_id'));
+		$this->assertFalse(df_db_column_exists(self::$TABLE, 'non_existent_column'));
 	}
 
 	/**
@@ -34,4 +30,7 @@ class DbColumn extends \PHPUnit\Framework\TestCase {
 		$this->expectException(\Exception::class);
 		df_db_column_exists('non_existent_table', 'customer_group_id');
 	}
+
+	/** @var string */
+	private static $TABLE = 'customer/customer_group';
 }
