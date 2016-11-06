8009950aa7296f1924ec273f088a5d3dcff6c026
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-07 00:27:32 +0300

    DF_PHPUNIT_STORE

--- app/code/local/Df/t.php
+++ app/code/local/Df/t.php
@@ -7,7 +7,8 @@ $base = substr($file, 0, strpos($file, 'app/code/local'));
 require_once "{$base}/app/Mage.php";
 Mage::setIsDeveloperMode(true);
 Mage::app('default');
-//Mage::app('en');
-Mage::dispatchEvent('default');
+// 2016-11-07
+// Пример настройки: SetEnv DF_PHPUNIT_STORE "en"
+Mage::dispatchEvent(@$_SERVER['DF_PHPUNIT_STORE'] ?: 'default');
 /** http://stackoverflow.com/a/4059399 */
 ob_start();
