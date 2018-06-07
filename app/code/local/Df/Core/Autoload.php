fe5e143376e119e77254b8b0725f2fec2f1dd606
Dmitry Fedyuk <dfediuk@gmail.com>
2018-06-07 16:00:24 +0600

    2.52.1: https://github.com/magento-russia/2/issues/15

--- app/code/local/Df/Core/Autoload.php
+++ app/code/local/Df/Core/Autoload.php
@@ -1,61 +1,52 @@
 <?php
 class Df_Core_Autoload extends Varien_Autoload {
 	/**
-	 * @param string $class
+	 * @param string $c
 	 * @return mixed|null
 	 */
-	public function autoload($class)
-	{
+	public function autoload($c) {
 		if ($this->_collectClasses) {
-			$this->_arrLoadedClasses[self::$_scope][]= $class;
+			$this->_arrLoadedClasses[self::$_scope][]= $c;
 		}
-		if ($this->_isIncludePathDefined) {
-			$classFile = $class;
-		} else {
-			$classFile = str_replace(' ', DIRECTORY_SEPARATOR, ucwords(str_replace('_', ' ', $class)));
-		}
-		$classFile.= '.php';
-		//echo $classFile;die();
-		/********************************
-		 * Начало заплатки
-		 *******************************/
-		ob_start();
-		/** @var mixed|null $result */
-		$result = include($classFile);
-		/** @var string|bool $errorMessage */
 		/**
-		 * Используем @, чтобы избежать сбоя «Failed to delete buffer zlib output compression».
-		 * Такой сбой у меня возник на сервере moysklad.magento-demo.ru
-		 * в другой точке программы при аналогичном вызове @see ob_get_clean.
+		 * 2018-06-07
+		 * 1) "«Warning: include(Phpseclib\Crypt\Base.php): failed to open stream:
+		 * No such file or directory app/code/local/Df/Core/Autoload.php on line 24»
+		 * при использовании PHP 7.2 и `mcrypt_compat`":
+		 * https://github.com/magento-russia/2/issues/15
+		 * 2) "Check if an include (or require) exists":
+		 * https://stackoverflow.com/a/13118081
+		 * http://php.net/manual/function.stream-resolve-include-path.php
 		 */
-		$errorMessage = @ob_get_clean();
-		if ((false !== $errorMessage) && ('' !== $errorMessage)) {
-			/** @var bool $isItBom */
-			$isItBom = false;
-			/** @var int $errorMessageLength */
-			$errorMessageLength = mb_strlen($errorMessage);
-			if (1 === $errorMessageLength) {
-				if (0 === mb_strlen(df_text()->bomRemove($errorMessage))) {
-					$isItBom = true;
+		if ($r =
+			!function_exists('stream_resolve_include_path')
+			|| stream_resolve_include_path(
+				$f = ($this->_isIncludePathDefined ? $c :
+					str_replace(' ', DIRECTORY_SEPARATOR, ucwords(str_replace('_', ' ', $c)))
+				) . '.php'
+			)
+		) {  /** @var string $f */
+			ob_start();
+			$r = include $f;
+			/**
+			 * Используем @, чтобы избежать сбоя «Failed to delete buffer zlib output compression».
+			 * Такой сбой у меня возник на сервере moysklad.magento-demo.ru
+			 * в другой точке программы при аналогичном вызове @see ob_get_clean.
+			 */
+			if (false !== ($m = @ob_get_clean()) && '' !== $m) { /** @var string|bool $m */
+				if (1 === mb_strlen($m) && 0 === mb_strlen(df_text()->bomRemove($m))) {
+					Mage::log("Дефект: файл начинается с символа BOM: $f");
+				}
+				else {
+					Mage::log(
+						"При загрузке интерпретатором PHP программного файла $f произошёл сбой.\n"
+						. "Сообщение интерпретатора: «{$m}»."
+					);
+					echo $m;
 				}
 			}
-			if (!$isItBom) {
-				Mage::log(
-					"При загрузке интерпретатором PHP программного файла %s произошёл сбой.\n"
-					. "Сообщение интерпретатора: «%s»."
-					,$classFile
-					,$errorMessage
-				);
-				echo $errorMessage;
-			}
-			else {
-				Mage::log('Дефект: файл начинается с символа BOM: ' . $classFile);
-			}
 		}
-		/********************************
-		 * Конец заплатки
-		 *******************************/
-		return $result;
+		return $r;
 	}
 
 	/**
